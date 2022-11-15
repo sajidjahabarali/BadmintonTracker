8a052b8bbada0bf48767c4ccb2cdd2cb2d881cc8
Sajid Jahabar Ali <sajidjahabarali@gmail.com>
2022-11-15 19:07:47 +0000

    replaced deprecated store function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,14 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
 import { loadFromLocalStorage, saveToLocalStorage } from "../common.utils";
 
-const localStorageKey = "globalState";
-const store = createStore(rootReducer, loadFromLocalStorage(localStorageKey));
+const LOCAL_STORAGE_KEY = "globalState";
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: loadFromLocalStorage(LOCAL_STORAGE_KEY),
+});
 store.subscribe(() => {
-  saveToLocalStorage(store.getState(), localStorageKey);
+  saveToLocalStorage(store.getState(), LOCAL_STORAGE_KEY);
 });
 
 export default store;
